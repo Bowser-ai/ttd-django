fd2fc42820f4f347284951172c8d888ea8688a1d
Jessica <martijnvermeulen1@gmail.com>
2020-02-22 17:33:08 +0100

    :q

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -3,10 +3,19 @@ from selenium.webdriver.common.keys import Keys
 import unittest
 import time
 
+
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def check_for_row_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = self.browser.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list(self):
         self.browser.get('http:localhost:8000')
@@ -24,24 +33,14 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         input_box = self.browser.find_element_by_id('id_new_item')
-        input_box.send_keys('Use Peacock feather to make a fly')
+        input_box.send_keys('Use Peacock feathers to make a fly')
         input_box.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = self.browser.find_elements_by_tag_name('tr')
-
-        self.assertIn('1: Buy Peacock Feathers', [row.text for row in rows],
-                f'list item did not appear in the table, item in table: {table.text}')
-
-        self.assertIn('2: Use Peacock feather to make a fly', [row.text for row in rows],
-                f'list item did not appear in the table, item in table: {table.text}')
-
+        self.check_for_row_list_table('1: Buy Peacock Feathers')
+        self.check_for_row_list_table('2: Use Peacock feathers to make a fly')
 
         self.fail('Finish the test')
-
-    def tearDown(self):
-        self.browser.quit()
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
