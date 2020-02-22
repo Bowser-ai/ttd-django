2054e688cc72f722a655c807875b33a433d8d0fb
Jessica <martijnvermeulen1@gmail.com>
2020-02-22 16:18:08 +0100

    build a test case to check the home-page template

--- list/views.py
+++ list/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_page(request):
-    return HttpResponse('<html><title>To-Do</title></html>')
+    return render(request, 'list/home.html')
 
