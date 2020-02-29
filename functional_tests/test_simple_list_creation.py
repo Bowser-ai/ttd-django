de623a4bd38f0e976137bd03baf5eb6adcd7f019
Jessica <martijnvermeulen1@gmail.com>
2020-02-29 17:49:54 +0100

    Moved Fts into their individual files

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,45 +1,8 @@
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from .base import FunctionalTest
 from selenium import webdriver
-from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
-import unittest
-import time
 
-MAX_WAIT = 10
-
-class NewVisitorTest(StaticLiveServerTestCase):
-
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-
-    def tearDown(self):
-        self.browser.quit()
-
-
-    def wait_for_row_list_table(self, row_text):
-        start_time = time.time()
-        while True:
-            try:
-
-                table = self.browser.find_element_by_id('id_list_table')
-                rows = self.browser.find_elements_by_tag_name('tr')
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except(AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
-
-    def test_layout_and_styling(self):
-        self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
-
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-                inputbox.location['x'] + inputbox.size['width'] / 2,
-                512,
-                delta=10
-                )
+class NewVisitorTest(FunctionalTest):
                         
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get(self.live_server_url)
@@ -62,7 +25,6 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.wait_for_row_list_table('2: Use Peacock feathers to make a fly')
         self.wait_for_row_list_table('1: Buy Peacock Feathers')
 
-        self.fail('Finish the test')
 
     def test_multiple_users_can_start_list_at_different_urls(self):
         self.browser.get(self.live_server_url)
@@ -93,4 +55,3 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertNotIn('Buy Peacock Feathers', body_text)
         self.assertNotIn('feathers to make a fly', body_text)
         self.assertIn('Buy Milk', body_text)
-
