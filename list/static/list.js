2194308a2781c8059a076e2f0c6016908991ef90
Jessica <martijnvermeulen1@gmail.com>
2020-03-28 13:15:49 +0100

    finished cpt javascript

--- list/static/list.js
+++ list/static/list.js
@@ -1,5 +1,9 @@
+window.SuperLists = {};
+
 function initialize() {
 	$('input[name="text"]').on('keypress', function() {
 		$('.has-error').hide();
 	});
 }
+
+window.SuperLists.initialize = initialize
--- list/static/tests/tests.js
+++ list/static/tests/tests.js
@@ -1,10 +1,10 @@
 QUnit.test( "errors should be hidden on input", function( assert ) {
-	initialize();
+	SuperLists.initialize();
 	$('input[name="text"]').trigger('keypress');
 	assert.equal($('.has-error').is(':visible'), false)
 });
 
 QUnit.test( "errors aren't hidden if there is no keypress", function( assert ) {
-	initialize();
+	SuperLists.initialize();
 	assert.equal($('.has-error').is(':visible'), true)
 });
