3c7894fe94816186adbb71d9f81863eaa35089de
Jessica <martijnvermeulen1@gmail.com>
2020-03-28 13:01:29 +0100

    added Qunit, jQuery and a small javascript to hide errors in the input form

---
+++ list/static/tests/tests.js
@@ -0,0 +1,10 @@
+QUnit.test( "errors should be hidden on input", function( assert ) {
+	initialize();
+	$('input[name="text"]').trigger('keypress');
+	assert.equal($('.has-error').is(':visible'), false)
+});
+
+QUnit.test( "errors aren't hidden if there is no keypress", function( assert ) {
+	initialize();
+	assert.equal($('.has-error').is(':visible'), true)
+});
