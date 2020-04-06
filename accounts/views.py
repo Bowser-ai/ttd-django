4af19092fd7c1527d7c0f4f253dfa7c33d73cd83
Jessica <martijnvermeulen1@gmail.com>
2020-04-06 20:08:54 +0200

    added test and view for login via token

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,19 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from django.core.mail import send_mail
 from django.contrib import messages
+
+from accounts.models import Token
 
 
 def send_login_email(request):
     email = request.POST.get('email', None)
+    token = Token.objects.create(email=email)
+    url = request.build_absolute_uri(
+            reverse('login') + '?token=' + str(token.uid)
+            )
+    message_body = f'Use this link to login:\n\n{url}'
     send_mail('Your login link for Superlists',
-            'Use this link to log in',
+            message_body,
             'noreply@superlists.com',
             [email]
             )
@@ -14,5 +21,8 @@ def send_login_email(request):
             request,
             'Check your email, we\'ve sent you a link you can use to log in.'
             )
+    return redirect('/')
+
+def login(request):
     return redirect('/')
 
